ef668557e7849ad88316dc76297554ce654d1db8
Edison Padilla <padilla.e@dhn.com.do>
2020-12-08 13:16:56 -0400

    #Edison Validacion

--- indice.js
+++ indice.js
@@ -1,19 +1,19 @@
 var Cantidad = 0;
 
-function onSubmit(){
+function onSubmit() {
     Cantidad = +document.getElementById("Cantidad").value;
-    if(Cantidad <= 0){
+    if (Cantidad <= 0) {
         alert("La cantidad no puede ser un numero negativo o cero")
         return;
     }
     document.getElementById("Cantidad").value = 0;
     let inputs = "<h3 class='text-center'>Ingrese las notas</h3>";
 
-    for(let i = 0; i < Cantidad; i++)
-       inputs += `
+    for (let i = 0; i < Cantidad; i++)
+        inputs += `
        <div class="row">
             <div class="col-md-auto"> 
-                <h4 class="font-weight-bolder">Materia ${i+1}:</h4>
+                <h4 class="font-weight-bolder">Materia ${i + 1}:</h4>
             </div>
             <div class="col">
                 <select class="form-control" id="Select${i}">
@@ -24,25 +24,47 @@ function onSubmit(){
                 </select>
             </div>
             <div class="col">
-                <input type="number" class="form-control" id="Input${i}" value="0" placeholder="Creditos">
+                <input type="number" class="form-control" id="Input${i}"  placeholder="Creditos">
             </div>
         </div><br>
        `
-       inputs += '<button class="btn btn-success btn-block" type="button" onclick="onSubmit2()">Enviar</button>'   
-       document.getElementById("Form2").innerHTML = inputs;
+    inputs += '<button class="btn btn-success btn-block" type="button" onclick="onSubmit2()">Enviar</button>'
+    document.getElementById("Form2").innerHTML = inputs;
+    console.log(Cantidad)
 }
 
-function onSubmit2(){
-    let valor = 0, creditos = 0;
-    for(let i = 0; i < Cantidad; i++){
-        creditos += +document.getElementById("Input"+i).value;
-        valor += +document.getElementById("Input"+i).value * +document.getElementById("Select"+i).value;
-    }
-    document.getElementById("Form2").innerHTML = ""
-    document.getElementById("mostrarPromedio").innerText = "Tu promedio es: " + (valor/creditos).toFixed(1);
-    document.getElementById("BtnReiniciar").style = "display: block;";
-    document.getElementById("BtnReiniciar").addEventListener("click", function () {
-        document.getElementById("BtnReiniciar").style = "display: none;";
-        document.getElementById("mostrarPromedio").innerText = "";
-    })
+
+
+function validateFiels() {
+
+    for (let i = 0; i < Cantidad; i++) {
+        let data =  document.getElementById(`Input${i}`).value ;
+        if(data == ""  || data.length  < 0 ){
+            return false;
+        } ;  
+    };
+
+    return true ;
+}
+
+function onSubmit2() {
+
+    if(validateFiels() == false){
+        alert("Faltan campos por llenar"); 
+        return ; 
+    };
+
+        let valor = 0, creditos = 0;
+        for (let i = 0; i < Cantidad; i++) {
+            creditos += +document.getElementById("Input" + i).value;
+            valor += +document.getElementById("Input" + i).value * +document.getElementById("Select" + i).value;
+        }
+        document.getElementById("Form2").innerHTML = ""
+        document.getElementById("mostrarPromedio").innerText = "Tu promedio es: " + (valor / creditos).toFixed(1);
+        document.getElementById("BtnReiniciar").style = "display: block;";
+        document.getElementById("BtnReiniciar").addEventListener("click", function () {
+            document.getElementById("BtnReiniciar").style = "display: none;";
+            document.getElementById("mostrarPromedio").innerText = "";
+        })
+
 }
