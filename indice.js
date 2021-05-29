fdda95505f4a0deb03199bbdbd5ca3e366313395
Yeyskalyn <yeyskalyn@gmail.com>
2021-05-29 15:14:01 -0400

    Añadí algunas mejoras de código

--- indice.js
+++ indice.js
@@ -1,72 +1,79 @@
-var Cantidad = 0;
+var cantidadMaterias = 0;
 
 function onSubmit() {
-    Cantidad = +document.getElementById("Cantidad").value;
-    if (Cantidad <= 0) {
-        alert("La cantidad no puede ser un numero negativo o cero")
+    cantidadInput = document.getElementById("cantidadMaterias");
+    formInput = document.getElementById("formulario");
+
+    cantidadMaterias = +cantidadInput.value;
+
+    if (cantidadMaterias <= 0) {
+        alert("La cantidad no puede ser un número negativo o cero")
         return;
     }
-    document.getElementById("Cantidad").value = 0;
-    let inputs = "<h3 class='text-center'>Ingrese las notas</h3>";
+    else {
+        cantidadInput.value = 0;
+        let inputs = "<h3 class='text-center mb-4'>Ingrese las notas</h3>";
 
-    for (let i = 0; i < Cantidad; i++)
-        inputs += `
-       <div class="row">
-            <div class="col-md-auto"> 
-                <h4 class="font-weight-bolder">Materia ${i + 1}:</h4>
-            </div>
-            <div class="col">
-                <select class="form-control" id="Select${i}">
-                    <option value="4">A</option>
-                    <option value="3">B</option>
-                    <option value="2">C de campeon</option>
-                    <option value="0">F</option>
-                </select>
-            </div>
-            <div class="col">
-                <input type="number" class="form-control" id="Input${i}"  placeholder="Creditos">
-            </div>
-        </div><br>
-       `
-    inputs += '<button class="btn btn-success btn-block" type="button" onclick="onSubmit2()">Enviar</button>'
-    document.getElementById("Form2").innerHTML = inputs;
-    console.log(Cantidad)
+        for (let i = 0; i < cantidadMaterias; i++)
+            inputs += `
+        <div class="row">
+                <div class="col-md-auto"> 
+                    <h4 class="font-weight-bolder">Materia ${i + 1}:</h4>
+                </div>
+                <div class="col">
+                    <select class="form-control" id="Select${i}">
+                        <option value="4">A</option>
+                        <option value="3">B</option>
+                        <option value="2">C de campeón</option>
+                        <option value="0">F</option>
+                    </select>
+                </div>
+                <div class="col">
+                    <input type="number" class="form-control" id="Input${i}"  placeholder="Créditos">
+                </div>
+            </div><br>
+        `;
+
+        inputs += '<button class="btn btn-success btn-block w-100" type="button" onclick="calcularIndice()">Calcular Índice</button>';
+        formInput.innerHTML = inputs;
+    }
 }
-
-
-
-
 
 function validateFiels() {
 
-    for (let i = 0; i < Cantidad; i++) {
-        let data =  document.getElementById(`Input${i}`).value ;
-        if(data == ""  || data.length  < 0 ){
+    for (let i = 0; i < cantidadMaterias; i++) {
+        let data = document.getElementById(`Input${i}`).value;
+        
+        if(data == "" || data.length < 0) {
             return false;
-        } ;  
+        }
+        else if (data <= 0){
+            return false;
+        }  
     };
 
     return true ;
 }
 
-function onSubmit2() {
+function calcularIndice() {
 
     if(validateFiels() == false){
-        alert("Faltan campos por llenar"); 
+        alert("Faltan campos por llenar || Los créditos deben ser mayor que cero."); 
         return ; 
     };
 
-        let valor = 0, creditos = 0;
-        for (let i = 0; i < Cantidad; i++) {
-            creditos += +document.getElementById("Input" + i).value;
-            valor += +document.getElementById("Input" + i).value * +document.getElementById("Select" + i).value;
-        }
-        document.getElementById("Form2").innerHTML = ""
-        document.getElementById("mostrarPromedio").innerText = "Tu promedio es: " + (valor / creditos).toFixed(1);
-        document.getElementById("BtnReiniciar").style = "display: block;";
-        document.getElementById("BtnReiniciar").addEventListener("click", function () {
-            document.getElementById("BtnReiniciar").style = "display: none;";
-            document.getElementById("mostrarPromedio").innerText = "";
-        })
+    let valor = 0, creditos = 0;
 
+    for (let i = 0; i < cantidadMaterias; i++) {
+        creditos += +document.getElementById("Input" + i).value;
+        valor += +document.getElementById("Input" + i).value * +document.getElementById("Select" + i).value;
+    }
+
+    document.getElementById("formulario").innerHTML = "";
+    document.getElementById("mostrarPromedio").innerText = "Tu promedio es: " + (valor / creditos).toFixed(1);
+    document.getElementById("btnReiniciar").style = "display: block;";
+    document.getElementById("btnReiniciar").addEventListener("click", function () {
+        document.getElementById("btnReiniciar").style = "display: none;";
+        document.getElementById("mostrarPromedio").innerText = "";
+    })
 }
